6d83066b8093fa95027eca34f79b27864f71c57b
francescovitale-dev <francescovitale1307@gmail.com>
2024-04-25 17:04:53 +0200

    .

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
-
+import { useTranslation } from 'react-i18next';
 import { SubHeading } from '../../components';
 import { images } from '../../constants';
 
-const FindUs = () => (
-  <div className="app__bg app__wrapper section__padding" id="contact">
-    <div className="app__wrapper_info">
-      <SubHeading title="Contact" />
-      <h1 className="headtext__cormorant" style={{ marginBottom: '3rem' }}>Find Us</h1>
-      <div className="app__wrapper-content">
-        <p className="p__opensans">Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 75G</p>
-        <p className="p__opensans">Email: info@nanáconvivial.de</p>
-        <p className="p__opensans">Phone: +49 1234567890</p>
-        <p className="p__cormorant" style={{ color: '#DCCA87', margin: '2rem 0' }}>Opening Hours</p>
-        <p className="p__opensans">Mon - Fri: 10:00 am - 02:00 am</p>
-        <p className="p__opensans">Sat - Sun: 10:00 am - 03:00 am</p>
-      </div>
-      {/* <button type="button" className="custom__button" style={{ marginTop: '2rem' }}>Visit Us</button> */}
-    </div>
+const FindUs = () => {
+  const { t } = useTranslation();
 
-    <div className="app__wrapper_img">
-      <img src={images.findus} alt="finus_img" />
+  return (
+    <div className="app__bg app__wrapper section__padding" id="contact">
+      <div className="app__wrapper_info">
+        <SubHeading title={t('contact.title')} />
+        <h1 className="headtext__cormorant" style={{ marginBottom: '3rem' }}>{t('contact.findUsTitle')}</h1>
+        <div className="app__wrapper-content">
+          <p className="p__opensans">{t('contact.address')}</p>
+          <p className="p__opensans" style={{ textTransform: 'none' }}>{t('contact.email')}</p>
+          <p className="p__opensans">{t('contact.phone')}</p>
+          <p className="p__cormorant" style={{ color: '#DCCA87', margin: '2rem 0' }}>{t('contact.openingHoursTitle')}</p>
+          <p className="p__opensans">{t('contact.openingHoursMonFri')}</p>
+          <p className="p__opensans">{t('contact.openingHoursSatSun')}</p>
+        </div>
+        {/* <button type="button" className="custom__button" style={{ marginTop: '2rem' }}>{t('contact.visitButton')}</button> */}
+      </div>
+
+      <div className="app__wrapper_img">
+        <img src={images.findus} alt="finus_img" />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default FindUs;
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,48 +1,46 @@
 import React from 'react';
 import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
-
-import { FooterOverlay, Newsletter } from '../../components';
+import { useTranslation } from 'react-i18next';
 import { images } from '../../constants';
 import './Footer.css';
 
-const Footer = () => (
-  <div className="app__footer section__padding" id="login">
-    <FooterOverlay />
-    <Newsletter />
+const Footer = () => {
+  const { t } = useTranslation();
 
-    <div className="app__footer-links">
-      <div className="app__footer-links_contact">
-        <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
-        <p className="p__opensans">+1 212-344-1230</p>
-        <p className="p__opensans">+1 212-555-1230</p>
-      </div>
+  return (
+    <div className="app__footer section__padding" id="login">
+      <div className="app__footer-links">
+        <div className="app__footer-links_contact">
+          <h1 className="app__footer-headtext">{t('footer.contactTitle')}</h1>
+          <p className="p__opensans">{t('footer.phone')}</p>
+          <p className="p__opensans">{t('footer.email')}</p>
+        </div>
 
-      <div className="app__footer-links_logo">
-        <img src={images.nana} alt="footer_logo" />
-        <p className="p__opensans">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
-        <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} />
-        <div className="app__footer-links_icons">
-          <FiFacebook />
-          <FiTwitter />
-          <FiInstagram />
+        <div className="app__footer-links_logo">
+          <img src={images.nana} alt="footer_logo" />
+          <p className="p__opensans">{t('footer.logoQuote')}</p>
+          <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} />
+          <div className="app__footer-links_icons">
+            <FiFacebook />
+            <FiTwitter />
+            <FiInstagram />
+          </div>
+        </div>
+
+        <div className="app__footer-links_work">
+          <h1 className="app__footer-headtext">{t('footer.workingHoursTitle')}</h1>
+          <p className="p__opensans">{t('footer.monFriHours')}</p>
+          <p className="p__opensans">08:00 am - 12:00 am</p>
+          <p className="p__opensans">{t('footer.satSunHours')}</p>
+          <p className="p__opensans">{t('footer.closed')}</p>
         </div>
       </div>
 
-      <div className="app__footer-links_work">
-        <h1 className="app__footer-headtext">Working Hours</h1>
-        <p className="p__opensans">Monday-Friday:</p>
-        <p className="p__opensans">08:00 am - 12:00 am</p>
-        <p className="p__opensans">Saturday-Sunday:</p>
-        <p className="p__opensans">07:00 am - 11:00 pm</p>
+      <div className="footer__copyright">
+        <p className="p__opensans">{t('footer.rightsReserved')}</p>
       </div>
     </div>
-
-    <div className="footer__copyright">
-      <p className="p__opensans">2024 Naná. All Rights reserved.</p>
-    </div>
-
-  </div>
-);
+  );
+};
 
 export default Footer;
