354d56986bd31a69fa8f2a0baa677bcbbfeb8750
francescovitale-dev <francescovitale1307@gmail.com>
2024-05-24 17:43:30 +0200

    menù

--- src/container/Menu/Menu.jsx
+++ src/container/Menu/Menu.jsx
@@ -4,15 +4,17 @@ import { images } from '../../constants';
 import './Menu.css';
 
 const Menu = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="app__bg-menu app__wrapper section__padding" id="menu">
       <div className="menu-section">
-        <h1 className='p__cormorant-menu'>MENÜ</h1>
+        <h1 className='p__cormorant-menu'>{t('menu.menuTitle')}</h1>
         <img src={images.mano} alt="about_mano" className="mano__img-menu" />
-        <h2 className='p__cormorant-menu-2'>Empfehlung des Küchenchefs</h2>
+        <h2 className='p__cormorant-menu-2'>{t('menu.menuDescription')}</h2>
         <div className="menu-card">
           <button className='menu-button' onClick={() => window.open('link-to-menu-document', '_blank')}>
-            La Carta
+          {t('menu.lacarta')}
           </button>
         </div>
       </div>
