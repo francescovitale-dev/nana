cc1e11fdab55c83d01a9fc28e071cb708b4d3cc9
francescovitale-dev <francescovitale1307@gmail.com>
2024-05-26 17:07:41 +0200

    .

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,46 +1,18 @@
-import bg from '../assets/bg.png';
 import chef from '../assets/chef.png';
-import N from '../assets/N.png';
-import gallery01 from '../assets/gallery01.png';
-import gallery02 from '../assets/gallery02.png';
-import gallery03 from '../assets/gallery03.png';
-import gallery04 from '../assets/gallery04.png';
-import knife from '../assets/knife.png';
-import menu from '../assets/menu.png';
 import overlaybg from '../assets/overlaybg.png';
-import spoon from '../assets/spoon.svg';
-import welcome from '../assets/welcome.png';
-import findus from '../assets/findus.png';
-// import laurels from '../assets/laurels.png';
 import sign from '../assets/sign.png';
-import quote from '../assets/quote.png';
 import logonb from '../assets/logonb.png';
 import logo from '../assets/logo.png';
-import nana from '../assets/nana.png';
 import mano from '../assets/mano.svg';
-import imgbackground from '../assets/imgbackground.png';
-import backgroundmenu from '../assets/backgroundmenu.svg';
+import imgbackground from '../assets/imgbackground.webp';
+import backgroundmenu from '../assets/backgroundmenu.webp';
 
 export default {
-  bg,
   chef,
-  N,
-  gallery01,
-  gallery02,
-  gallery03,
-  gallery04,
-  knife,
-  menu,
   overlaybg,
-  spoon,
-  welcome,
-  findus,
-  // laurels,
   sign,
-  quote,
   logonb,
   logo,
-  nana,
   mano,
   imgbackground,
   backgroundmenu
--- src/container/index.js
+++ src/container/index.js
@@ -2,10 +2,9 @@ import AboutUs from './AboutUs/AboutUs';
 import Chef from './Chef/Chef';
 import FindUs from './Findus/FindUs';
 import Footer from './Footer/Footer';
-import Gallery from './Gallery/Gallery';
+// import Gallery from './Gallery/Gallery';
 import Header from './Header/Header';
 import Intro from './Intro/Intro';
-import Laurels from './Laurels/Laurels';
 import Menu from './Menu/Menu';
 
 export {
@@ -13,9 +12,8 @@ export {
   Chef,
   FindUs,
   Footer,
-  Gallery,
+  // Gallery,
   Header,
   Intro,
-  Laurels,
   Menu,
 };
