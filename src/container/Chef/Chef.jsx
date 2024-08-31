ab2cb5f575b9feabd2f8e3b42fdeaab721b66f22
francescovitale-dev <francescovitale1307@gmail.com>
2024-08-31 16:44:57 +0200

    chef animation

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,34 +1,60 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { motion } from 'framer-motion';
 import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => {
   const { t } = useTranslation();
 
+  const contentVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { 
+      opacity: 1, 
+      y: 0, 
+      transition: { duration: 0.8 } 
+    }
+  };
+
   return (
     <div className="app__bg app__wrapper section__padding">
-      <div className="app__chef-container">
-        {/* <div className="app__chef-img">
-          <img src={images.chef} alt="chef_image" className="chef_img" />
-        </div> */}
+      <motion.div 
+        className="app__chef-container"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.3 }}
+        variants={contentVariants}
+      >
         <div className="app__chef-description">
           <h1 className="headtext__cormorant">{t('chef.beliefTitle')}</h1>
-          <img src={images.mano} alt="about_mano" className="mano__img" />
+          <motion.img 
+            src={images.mano} 
+            alt="about_mano" 
+            className="mano__img"
+            whileHover={{ scale: 1.05 }}
+            transition={{ type: "spring", stiffness: 300 }}
+          />
 
           <div className="app__chef-content">
-            {/* <img src={images.quote} alt="quote_image" /> */}
             <div className="app__chef-content_quote">
               <p className="p__opensans">{t('chef.quote')}</p>
             </div>
           </div>
 
           <div className="app__chef-sign">
-            <img src={images.sign} alt="sign_image" />
+            <motion.img 
+              src={images.sign} 
+              alt="Salvo Maggiore signature" 
+              className="signature-img"
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 4 }}
+              viewport={{ once: true }}
+            />
             <p className="abeezee-regular">{t('chef.role')}</p>
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
